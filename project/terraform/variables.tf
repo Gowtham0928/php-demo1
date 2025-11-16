ff1266f38e35914640d23dcf7f708dbf51a8e289
Gowtham0928 <103296844+Gowtham0928@users.noreply.github.com>
2025-11-16 10:09:09 +0530

    Update variables.tf

--- project/terraform/variables.tf
+++ project/terraform/variables.tf
@@ -15,6 +15,6 @@ variable "zone" {
 
 variable "docker_image" {
   type        = string
-  default     = "us-central1-docker.pkg.dev/hari-chandu-anunay/demo-1/kandula@sha256:0c05b4a9f4c29e2d70f230f9f9cc34698ae9acee3e549fb036969fd892438529"
+  default     = "us-central1-docker.pkg.dev/hari-chandu-anunay/demo-1/kandula:v2"
 }
 
