f2316e114f6b8ed842ff5507a8ff178e102661a5
Gowtham0928 <103296844+Gowtham0928@users.noreply.github.com>
2025-11-17 10:57:25 +0530

    Update index.php

--- project/application/index.php
+++ project/application/index.php
@@ -9,7 +9,7 @@ echo "<title>PHP Automated Deployment</title>";
 echo "</head>";
 echo "<body>";
 echo "<div class='container'>";
-echo "<h1>🚀 PHP Application Deployed Successfully!</h1>";
+echo "<h1>🚀 PHP Application Deployed Successfully pro!</h1>";
 echo "<p>This application is deployed through Jenkins → Artifact Registry → MIG → ALB.</p>";
 echo "<p>Everything is fully automated!</p>";
 echo "</div>";
