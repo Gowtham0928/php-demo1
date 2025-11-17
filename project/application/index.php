87a0c6e0f68050fe03d7c8d40d51c5688379a5d9
Gowtham0928 <103296844+Gowtham0928@users.noreply.github.com>
2025-11-17 12:03:22 +0530

    Fix typo in deployment success message

--- project/application/index.php
+++ project/application/index.php
@@ -9,7 +9,7 @@ echo "<title>PHP Automated Deployment</title>";
 echo "</head>";
 echo "<body>";
 echo "<div class='container'>";
-echo "<h1>🚀 PHP Application Deployed Successfully pro env!</h1>";
+echo "<h1>🚀 PHP Application Deployed Successfully pro en!</h1>";
 echo "<p>This application is deployed through Jenkins → Artifact Registry → MIG → ALB.</p>";
 echo "<p>Everything is fully automated!</p>";
 echo "</div>";
